abd4d699fb2e943827c4d332b1e6ba8e1eab716c
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2024-05-27 14:19:50 -0700

    Init reporting service

---
+++ reporting-service/src/test/java/com/bikerackapp/reporting/ReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikerackapp.reporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
