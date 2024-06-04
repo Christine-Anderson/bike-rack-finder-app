76e20216f2f9fd14a32e90b0b416d6ec330d0bcc
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2024-06-03 20:15:04 -0700

    Init BikeRackService

---
+++ bike-rack-service/src/main/java/com/bikerackapp/bike_rack_service/BikeRackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bikerackapp.bike_rack_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeRackServiceApplication.class, args);
+	}
+
+}
---
+++ bike-rack-service/src/test/java/com/bikerackapp/bike_rack_service/BikeRackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikerackapp.bike_rack_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
