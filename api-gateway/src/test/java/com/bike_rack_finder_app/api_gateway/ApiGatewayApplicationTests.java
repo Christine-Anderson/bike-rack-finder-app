7ad1cbe67dae06f8ef763c3e097ddd0ff08eedbc
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2024-06-12 17:25:50 -0700

    Init API gateway

---
+++ api-gateway/src/test/java/com/bike_rack_finder_app/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bike_rack_finder_app.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
