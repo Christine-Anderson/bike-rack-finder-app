c6bf1e262762a2c182c15c5d8687b9a44c2ef666
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2024-07-02 16:36:03 -0700

    Add circuit breaker to API gateway

--- backend/api-gateway/src/main/java/com/bike_rack_finder_app/api_gateway/Routes.java
+++ backend/api-gateway/src/main/java/com/bike_rack_finder_app/api_gateway/Routes.java
@@ -1,12 +1,17 @@
 package com.bike_rack_finder_app.api_gateway;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 
 @Configuration
 public class Routes {
@@ -17,6 +22,10 @@ public class Routes {
                 .route(RequestPredicates.POST("/report"), HandlerFunctions.http("http://localhost:8080"))
                 .route(RequestPredicates.GET("/report/{reportId}"), HandlerFunctions.http("http://localhost:8080"))
                 .route(RequestPredicates.PUT("/report/{reportId}"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker(
+                        "reportingServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                ))
                 .build();
     }
 
@@ -26,6 +35,18 @@ public class Routes {
                 .route(RequestPredicates.GET("/bikeRack"), HandlerFunctions.http("http://localhost:8081"))
                 .route(RequestPredicates.GET("/bikeRack/{rackId}"), HandlerFunctions.http("http://localhost:8081"))
                 .route(RequestPredicates.PUT("/bikeRack/{rackId}/rating"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker(
+                        "bikeRackServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                ))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return route("fallbackRoute")
+                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Service Unavailable, please try again later"))
                 .build();
     }
 }
